c716f26750f276f4373d286106129628ae56feb8
arai <niimaru09@gmail.com>
2023-10-23 13:56:14 +0900

    2nd class

---
+++ js/test.js
@@ -0,0 +1,12 @@
+let a = 100;
+let b = 20;
+const d = 10;
+let c = a + b + d;
+
+if (a > b) {
+    console.log('a is bigger than b.')
+} else {
+    console.log('b is bigger than a.')
+}
+
+window.alert("hoge")
